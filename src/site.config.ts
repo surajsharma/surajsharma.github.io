7d267784f7fe08ac3d28751c45199323f7e5d5bb
Suraj Sharma <suraj@evenzero.in>
2026-05-12 10:46:25 +0530

    fix: metadata

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,11 +14,11 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Suraj Sharma",
+	author: "evenzero",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "evenzero",
 	// Meta property used as a default description meta property
-	description: "Suraj Sharma - Full-stack software engineer with 10,000+ hours of experience in building pixel-perfect frontends, scalable APIs, and cloud-native microservices. Specializing in React, TypeScript, Node.js, and modern web development.",
+	description: "hyperreal software foundry",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
